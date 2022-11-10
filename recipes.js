61e07cca7868ab1d2193ed6f63ae6dc3d7eb48e5
TanakaLatias <109828366+TanakaLatias@users.noreply.github.com>
2022-11-10 11:36:03 +0900

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,259 @@
+
+
+data = [
+  {
+    title: 'Baked Cheesecake',
+    content: `Baked cheesecake is the classic type of cheesecake, originating when fridges weren’t yet available. As a result, refrigerating anything was not an option. Instead, desserts were generally baked in the oven. Baked cheesecake is a great way to use up those eggs. If you enjoy eggs, baked cheesecake is certainly worth considering.
+    `,
+    image: `./images/cakeWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: 'Select a pie pan whose inside top dimension is at least 9", and whose height is at least 1 1/4". Preheat the oven to 350°F.',
+    },
+    {
+      title: '',
+      content: "To make the crust: Stir together all of the crust ingredients, mixing until thoroughly combined.",
+    },
+    ],
+    ingredients: [
+    "- 1 1/2 cups (128g) graham cracker crumbs, 9 to 10 whole graham crackers, crushed",
+    "- 1/4 cup (28g) confectioners' sugar",
+    "- 5 1/3 tablespoons (74g) butter, melted",
+    ],
+  },
+  {
+    title: "Souffle Cheesecake",
+    content: "Souffle Cheesecake is a popular cheesecake sold at western-style dessert shops in Japan. As you can guess from the name, it is a light and airy soufflé-like dessert as opposed to dense and thick American cheesecake.",
+    image: `./images/chocoWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "Preheat oven at 320 F (160 C). Measure all the ingredients. Separate eggs into yolks and whites. Sift cake flour. Grate lemon peel. Prepare hot water for water bath."
+    },
+    {
+      title: '',
+      content: 'Coat a 6" round cake pan with melted butter. Place parchment paper in the pan, on the bottom and side. The parchment paper should also be coated with butter very well (the melted butter is not in the 2 Tbsp measurement). Sprinkle powdered sugar over the butter-coated parchment paper.',
+    },
+    ],
+    ingredients: [
+    '150 g (5.3 oz) cream cheese, room temperature',
+    '2 Tbsp butter, room temperature',
+    '90 g (3.2 oz) sugar',
+    ],
+  },
+  {
+    title: "No-bake Cheesecake",
+    content: "No-bake cheesecake is exactly what the name suggests. The graham cracker crust and cheesecake filling never see the inside of the oven, so it’s a fantastic dessert when the weather’s warm or when your oven is crowded. Or when you don’t feel like baking. No-bake cheesecake is a lot easier than baked cheesecake.",
+    image: `./images/cakeWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "Put graham crackers in a large resealable plastic bag, and crush them with a rolling pin until very fine crumbs form."
+    },
+    {
+      title: '材料の確認',
+      content: "Pour crumbs into a medium bowl; stir in sugar. Add butter, and stir until well combined.",
+    },
+    ],
+    ingredients: [
+    "2 packages (20 sheets) graham crackers",
+    "11 tablespoons (1 3/8 sticks) unsalted butter, melted",
+    "2 tablespoons sugar",
+    ],
+  },
+  {
+    title: "New York Cheesecake",
+    content: "A New York Cheesecake is the larger, richer and more indulgent cousin of the traditional cheesecake. We all know how American’s like things bigger and better in the USA and cheesecakes are no exception (not that we’re complaining).",
+    image: `./images/chocoWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "Preheat oven to 350 degrees F (175 degrees C)."
+    },
+    {
+      title: '',
+      content: "Mix together the cracker crumbs, sugar, and butter in a bowl until evenly moistened. Press into the bottom and 1/2 inch up the sides of a 9-inch springform pan.",
+    },
+    ],
+    ingredients: [
+    "1 ½ cups graham cracker crumbs",
+    "¼ cup white sugar",
+    "⅓ cup butter, melted",
+    ],
+  },
+  {
+    title: "Camembert Cheesecake",
+    content: "Camembert is a moist, soft, creamy, surface-ripened cow 's milk cheese. It was first made in the late 18th century in Camembert, Normandy, in northwest France.",
+    image: `./images/cakeWhite.png`,
+    steps: [
+    {
+      title: "Make the Cheesecake Filling",
+      content: "Next up is the filling of the cheesecake. You want to use room-temperature cream cheese to prevent a lumpy cheesecake. I also recommend the full-fat stuff for the creamiest cheesecake!"
+    },
+    {
+      title: "",
+      content: "Add sugar and a little bit of flour to the cream cheese and combine it all together. The flour is a nice little touch to give you a creamier textured cheesecake that isn’t quite so dense.",
+    },
+    ],
+    ingredients: [
+    "24 ounces (678g) cream cheese, room temperature",
+    "1 cup (207g) sugar",
+    "3 tbsp (24g) all purpose flour",
+    ],
+  },
+  {
+    title: "Ricotta Cheesecake",
+    content: "Ricotta is an Italian whey cheese made from sheep, cow, goat, or Italian water buffalo milk whey left over from the production of other cheeses. Like other whey cheeses, it is made by coagulating the proteins that remain after the casein has been used to make cheese, notably albumin and globulin.",
+    image: `./images/chocoWhite.png`,
+    steps: [
+    {
+      title: "",
+      content: "Preheat oven to 350F/180C. Wrap the bottom and sides of a 9-inch springform pan with one large piece of foil, then repeat with another piece. This will ensure that water from the water bath doesn’t seep into the cake."
+    },
+    {
+      title: "",
+      content: "In a medium bowl mix crumbs and melted butter until evenly combined and moistened. Press mixture into pan. You can use the bottom of a glass to press it into an even layer. Bake for 8-10 minutes, until golden brown. Allow to cool completely on a wire rack before adding the filling. Meanwhile, prepare the filling.",
+    },
+    ],
+    ingredients: [
+    "8 oz. (225g) full-fat cream cheese, at room temperature",
+    "540 g/19 oz whole-milk ricotta cheese , drained**, at room temperature",
+    "1 cup (200g) granulated sugar",
+    ],
+  },
+  {
+    title: "Tiramisu",
+    content: "An Italian dessert consisting of layers of sponge cake soaked in coffee and brandy or liqueur with powdered chocolate and mascarpone cheese.",
+    image: `./images/cakeWhite.png`,
+    steps: [
+    {
+      title: "This is how you will make the Sabayon",
+      content: "When you are ready to make the sabayon place about one inch of water in a small pot and bring to a boil. Reduce the heat to simmer and place a bowl on top of the small pot so it's sitting on the pot without touching the water."
+    },
+    {
+      title: "Now Begin Making the Sabayon",
+      content: "Add the egg yolks and sugar to the bowl, whipping to mix the ingredients together. Place the bowl on the small pot of simmering water. Using a whisk to mix, whip the egg/sugar mixture constantly for about 10 minutes. This will cook the eggs. The mixture will increase in volume as it thickens. This is your sabayon. After you have finished whipping the sabayon, remove the bowl from the heat and continue to whip yolks to help it cool. The mixture should be thick and a lemon yellow color. Allow to cool briefly before mixing in mascarpone.",
+    },
+    ],
+    ingredients: [
+    "6 large egg yolks (approx. ½ cup of yolks)",
+    "1 cup sugar superfine or caster if possible",
+    "1¼ cup mascarpone cheese (room temperature)",
+    ],
+  },
+  {
+    title: "Blueberry Cheesecake",
+    content: "The edible blue or blackish berry of any of several North American plants (genus Vaccinium) of the heath family also : a low or tall shrub producing these berries.",
+    image: `./images/chocoWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "Using electric mixer, beat cream cheese and sugar in large bowl until will blended. Beat in flour. Add eggs 1 at a time, beating just until combined. Beat in sour cream, milk and vanilla. Pour filling into crust."
+    },
+    {
+      title: '',
+      content: "Place springform pan in large roasting pan. Pour enough hot water into roasting pan to come 1 inch up sides of pan. Bake cheesecake until just set in center and top is slightly puffed and golden brown, about 1 hour. Turn off oven; keep door closed. Let cheesecake stand in oven 1 hour. Remove cheesecake from roasting pan. Refrigerate until cold, at least 6 hours. Cover; refrigerate overnight.",
+    },
+    ],
+    ingredients: [
+    "4 8-ounce packages cream cheese, room temperature",
+    "1 1/2 cups sugar",
+    "1/4 cup all purpose flour",
+    ],
+  },
+  {
+    title: "Yogurt Cake",
+    content: "A semi-solid food prepared from milk fermented by added bacteria, often sweetened and flavoured:",
+    image: `./images/cakeWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "reheat oven to 350°F. Spray the interior of a 6-inch round springform pan with cooking spray and then line it with a large sheet of parchment paper so that it completely covers the interior of the cake pan and comes up slightly above the pan. Press and crease the parchment paper as needed so that it sticks to the interior edges. You want to use a single sheet of parchment paper. It is okay that the parchment paper will be wrinkled and uneven around the edges. Just try your best to press it flatly against the sides and bottom. This is supposed to be a rustic-looking cake so the cake edges will not be perfectly round."
+    },
+    {
+      title: '',
+      content: "In a large bowl add yogurt. If you are using sugar (see notes), add it now and whisk it in. Add eggs, one at a time, whisking after each addition until the egg is completely blended into the yogurt. When you are finished, you should have a smooth batter.",
+    },
+    ],
+    ingredients: [
+    "1 1/2 cups (375 g) vanilla yogurt",
+    "4 large eggs",
+    "5 tbsp (44 g) cornstarch",
+    ],
+  },
+  {
+    title: "Cream Cheese Mousse",
+    content: "a sweet or savoury dish made as a smooth, light mass in which the main ingredient is whipped with cream and egg white",
+    image: `./images/ChocoWhite.png`,
+    steps: [
+    {
+      title: '',
+      content: "In a bowl, whip the cream. Set aside."
+    },
+    {
+      title: '',
+      content: "In a second bowl, beat the cream cheese using an electric mixer. Fold in the sugar and lemon juice, continuing to beat the mixture until smooth and homogeneous. Add the whipped cream, folding it gently into the mixture, using a spatula.",
+    },
+    ],
+    ingredients: [
+    "3 tbsp	whipping cream 35%",
+    "1/2 cup	cream cheese",
+    "3 tbsp	sugar",
+    ],
+  },
+  ];
+
+
+const menuBox = document.getElementById('menuBox')
+
+for (let i = 0; i < data.length; i++) {
+
+  const menuPage = document.createElement('div')
+  menuPage.classList.add('menuPage')
+  menuPage.setAttribute('id', i)
+  menuBox.appendChild(menuPage)
+
+  const menuPageImgBox = document.createElement('div')
+  menuPageImgBox.classList.add('menuPageImgBox')
+  menuPage.appendChild(menuPageImgBox)
+
+  const menuPageImgImg = document.createElement('img')
+  menuPageImgImg.classList.add('menuPageImgImg')
+  menuPageImgImg.setAttribute('src', data[i].image)
+  menuPageImgBox.appendChild(menuPageImgImg)
+
+  const menuPageParaBox = document.createElement('div')
+  menuPageParaBox.classList.add('menuPageParaBox')
+  menuPage.appendChild(menuPageParaBox)
+
+  const menuPageH3 = document.createElement('h3')
+  menuPageH3.classList.add('menuPageH3')
+  menuPageH3.textContent = data[i].title
+  menuPageParaBox.appendChild(menuPageH3)
+
+  const menuPageP = document.createElement('p')
+  menuPageP.classList.add('menuPageP')
+  menuPageP.textContent = data[i].content
+  menuPageParaBox.appendChild(menuPageP)
+
+  const number = document.createElement('p')
+  number.classList.add('number')
+  number.textContent = i + 1
+  menuPage.appendChild(number)
+
+  menuPage.addEventListener('mouseover', () => {
+    number.textContent = ''
+  })
+
+  menuPage.addEventListener('mouseleave', () => {
+    number.textContent = i + 1
+  })
+
+  menuPage.addEventListener('click', ()=> {
+    location.href= `menu.html?${i}`;
+  })
+
+}
+
+
